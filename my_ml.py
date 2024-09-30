e093918b38058c27e7dcddbbc2ca0b057a0bdf04
HarjotIris <harjotsingh22nov@gmail.com>
2024-09-30 22:46:15 +0530

    Learned Model Validation
    I comment some old code when I learn new things, I do not delete the code a lot because I want to document my learnings.

--- my_ml.py
+++ my_ml.py
@@ -1,19 +1,13 @@
-# import pandas
+# Code you have previously used to load data
 import pandas as pd
 
-# save filepath to variable for easier access
+# Code you have previously used to load data
 iowa_file_path = r"C:\Users\HP\train.csv"
-# read the data and store the data in DataFrame called home_data
+
 home_data = pd.read_csv(iowa_file_path)
 
-''' 
-Set up code checking
-from learntools.core import binder
-binder.bind(globals())
-from learntools.machine_learning.ex3 import *
-'''
+
 """
-print("Setup complete")
 print(home_data.describe())
 print(home_data.columns)
 """
@@ -30,12 +24,30 @@ x = home_data[feature_names]
 
 #print(x['LotArea'], x['YearBuilt'])
 
-# Import the DecisionTreeRegressor class from sklearn
+#Import the DecisionTreeRegressor class from sklearn
 from sklearn.tree import DecisionTreeRegressor
+"""
 iowa_model = DecisionTreeRegressor(random_state = 1)
 iowa_model.fit(x, y)
 predictions = iowa_model.predict(x)
-#print(predictions[:5])
+print(predictions[:5])
 
-comparison = pd.DataFrame({"Actual": y, "Predicted": predictions})
+comparison = pd.DataFrame( {"Actual": y, "Predicted": predictions})
 print(comparison.head())
+"""
+
+# Model Validation begins
+from sklearn.model_selection import train_test_split
+train_x, val_x, train_y,val_y = train_test_split(x, y, random_state = 1)
+iowa_model = DecisionTreeRegressor(random_state = 1)
+iowa_model.fit(train_x, train_y)
+
+val_predictions = iowa_model.predict(val_x)
+
+comparison = pd.DataFrame({"Actual": val_y, "Predicted": val_predictions})
+print(comparison.head())
+
+# Calculating the mean absolute error in validation data
+from sklearn.metrics import mean_absolute_error
+val_mae = mean_absolute_error(val_y, val_predictions)
+print(val_mae)
